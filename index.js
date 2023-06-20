5c86f0c025dec50dd2f4ab612c27f85fb95cf9b3
osman <harmless068@gmail.com>
2023-06-20 21:13:45 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
-// Write your code here
+let num1=31;
+let num2=2;
+let multiply =num1 * num2
+console.log(multiply); //outout: 62
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+let num3=22;
+let num4=6;
+let mod=num3 % num4;
+console.log(mod); output:4
+let num5 = 10;
+let num6 = 15;
+let num7 = 20;
+let max = Math.max(num5, num6, num7);
+console.log(max);  // Output: 20           
